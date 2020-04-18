426e21c5d81fd63bc9e1352b5ca801979c813dda
cyq <dz1933003@smail.nju.edu.cn>
2020-04-18 09:43:02 +0000

    create dpsk module

---
+++ src/dpsk/helper/dpsk-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef DPSK_HELPER_H
+#define DPSK_HELPER_H
+
+#include "ns3/dpsk.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* DPSK_HELPER_H */
+
---
+++ src/dpsk/model/dpsk.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef DPSK_H
+#define DPSK_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* DPSK_H */
+
