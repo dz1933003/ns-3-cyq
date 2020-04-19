29dc4bc62c3569578eb194d366ee4f8ee854be89
cyq <dz1933003@smail.nju.edu.cn>
2020-04-19 05:41:31 +0000

    add dpsk-helper

--- src/dpsk/helper/dpsk-helper.cc
+++ src/dpsk/helper/dpsk-helper.cc
@@ -1,11 +1,67 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Yanqing Chen  <shellqiqi@outlook.com>
+ */
 
 #include "dpsk-helper.h"
+#include "ns3/log.h"
+#include "ns3/node.h"
+#include "ns3/names.h"
 
 namespace ns3 {
 
-/* ... */
+NS_LOG_COMPONENT_DEFINE ("DPSKHelper");
 
-
+DPSKHelper::DPSKHelper ()
+{
+  NS_LOG_FUNCTION_NOARGS ();
+  m_deviceFactory.SetTypeId ("ns3::DPSK");
 }
 
+void
+DPSKHelper::SetDeviceAttribute (std::string n1, const AttributeValue &v1)
+{
+  NS_LOG_FUNCTION_NOARGS ();
+  m_deviceFactory.Set (n1, v1);
+}
+
+Ptr<DPSK>
+DPSKHelper::Install (Ptr<Node> node, NetDeviceContainer c)
+{
+  NS_LOG_FUNCTION_NOARGS ();
+  NS_LOG_LOGIC ("**** Install DPSK on node " << node->GetId ());
+
+  Ptr<DPSK> dpsk = m_deviceFactory.Create<DPSK> ();
+  node->AddDevice (dpsk);
+
+  for (NetDeviceContainer::Iterator iter = c.Begin (); iter != c.End (); ++iter)
+    {
+      NS_LOG_LOGIC ("**** Add Port " << *iter << " to DPSK");
+      dpsk->AddDevice (*iter);
+    }
+
+  return dpsk;
+}
+
+Ptr<DPSK>
+DPSKHelper::Install (std::string nodeName, NetDeviceContainer c)
+{
+  NS_LOG_FUNCTION_NOARGS ();
+  Ptr<Node> node = Names::Find<Node> (nodeName);
+  return Install (node, c);
+}
+
+} // namespace ns3
--- src/dpsk/helper/dpsk-helper.h
+++ src/dpsk/helper/dpsk-helper.h
@@ -1,14 +1,82 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Yanqing Chen  <shellqiqi@outlook.com>
+ */
+
 #ifndef DPSK_HELPER_H
 #define DPSK_HELPER_H
 
 #include "ns3/dpsk.h"
+#include "ns3/net-device-container.h"
+#include "ns3/object-factory.h"
 
 namespace ns3 {
 
-/* ... */
+class Node;
+class AttributeValue;
 
-}
+/**
+ * \ingroup dpsk
+ * \brief Add capability to DPSK device management
+ */
+class DPSKHelper
+{
+public:
+  /*
+   * Construct a DPSKHelper
+   */
+  DPSKHelper ();
+
+  /**
+   * Set an attribute on each ns3::DPSK created by
+   * DPSKHelper::Install
+   *
+   * \param n1 the name of the attribute to set
+   * \param v1 the value of the attribute to set
+   */
+  void SetDeviceAttribute (std::string n1, const AttributeValue &v1);
+
+  /**
+   * This method creates an ns3::DPSK with the attributes
+   * configured by DPSKHelper::SetDeviceAttribute, adds the device
+   * to the node, and attaches the given NetDevices as ports of the
+   * DPSK.
+   *
+   * \param node The node to install the DPSK in
+   * \param c Container of NetDevices to add as DPSK managed ports
+   * \returns The DPSK virtual device
+   */
+  Ptr<DPSK> Install (Ptr<Node> node, NetDeviceContainer c);
+
+  /**
+   * This method creates an ns3::DPSK with the attributes
+   * configured by DPSKHelper::SetDeviceAttribute, adds the device
+   * to the node, and attaches the given NetDevices as ports of the
+   * DPSK.
+   *
+   * \param nodeName The name of the node to install the device in
+   * \param c Container of NetDevices to add as DPSK managed ports
+   * \returns The DPSK virtual device
+   */
+  Ptr<DPSK> Install (std::string nodeName, NetDeviceContainer c);
+
+private:
+  ObjectFactory m_deviceFactory; //!< Object factory
+};
+
+} // namespace ns3
 
 #endif /* DPSK_HELPER_H */
-
