c2ee148027f6223db8aca2de10c8a63086dc9510
cyq <dz1933003@smail.nju.edu.cn>
2020-05-11 14:54:20 +0000

    init pfc module

---
+++ src/pfc/helper/pfc-helper.cc
@@ -0,0 +1,11 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+
+#include "pfc-helper.h"
+
+namespace ns3 {
+
+/* ... */
+
+
+}
+
---
+++ src/pfc/helper/pfc-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef PFC_HELPER_H
+#define PFC_HELPER_H
+
+#include "ns3/pfc.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* PFC_HELPER_H */
+
