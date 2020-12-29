e95ea1fd664102b14fe80cfb0b49600bceeba552
cyq <dz1933003@smail.nju.edu.cn>
2020-12-29 14:49:44 +0800

    add no pfc switch port

---
+++ src/pfc/model/nopfc-switch-port.cc
@@ -0,0 +1,233 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2020 Nanjing University
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Yanqing Chen  <shellqiqi@outlook.com>
+ */
+
+#include "nopfc-switch-port.h"
+
+#include "ns3/log.h"
+#include "ns3/uinteger.h"
+#include "ns3/pointer.h"
+#include "ns3/ethernet-header.h"
+#include "ns3/ipv4-header.h"
+#include "ns3/cbfc-header.h"
+#include "ns3/dpsk-net-device.h"
+#include "pfc-switch-tag.h"
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("NoPfcSwitchPort");
+
+NS_OBJECT_ENSURE_REGISTERED (NoPfcSwitchPort);
+
+TypeId
+NoPfcSwitchPort::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::NoPfcSwitchPort")
+                          .SetParent<Object> ()
+                          .SetGroupName ("NoPfcSwitchPort")
+                          .AddConstructor<NoPfcSwitchPort> ();
+  return tid;
+}
+
+NoPfcSwitchPort::NoPfcSwitchPort ()
+    : m_nQueues (0),
+      m_lastQueueIdx (0),
+      m_nInQueueBytes (0),
+      m_nInQueuePackets (0),
+      m_nTxBytes (0),
+      m_nRxBytes (0)
+{
+  NS_LOG_FUNCTION (this);
+  m_name = "NoPfcSwitchPort";
+}
+
+NoPfcSwitchPort::~NoPfcSwitchPort ()
+{
+  NS_LOG_FUNCTION (this);
+}
+
+void
+NoPfcSwitchPort::SetupQueues (uint32_t n)
+{
+  NS_LOG_FUNCTION (n);
+  CleanQueues ();
+  m_nQueues = n;
+  for (uint32_t i = 0; i <= m_nQueues; i++) // with another control queue
+    {
+      m_queues.push_back (std::queue<Ptr<Packet>> ());
+      m_inQueueBytesList.push_back (0);
+      m_inQueuePacketsList.push_back (0);
+    }
+}
+
+void
+NoPfcSwitchPort::CleanQueues ()
+{
+  NS_LOG_FUNCTION_NOARGS ();
+  m_queues.clear ();
+  m_inQueueBytesList.clear ();
+  m_inQueuePacketsList.clear ();
+}
+
+void
+NoPfcSwitchPort::SetDeviceDequeueHandler (DeviceDequeueNotifier h)
+{
+  m_mmuCallback = h;
+}
+
+Ptr<Packet>
+NoPfcSwitchPort::Transmit ()
+{
+  NS_LOG_FUNCTION_NOARGS ();
+
+  uint32_t qIndex;
+  Ptr<Packet> p = Dequeue (qIndex);
+
+  if (p == 0)
+    return 0;
+
+  // Notify switch dequeue event
+  m_mmuCallback (m_dev, p, qIndex);
+
+  PfcSwitchTag tag;
+  p->RemovePacketTag (tag);
+
+  m_nTxBytes += p->GetSize ();
+
+  return p;
+}
+
+bool
+NoPfcSwitchPort::Send (Ptr<Packet> packet, const Address &source, const Address &dest,
+                       uint16_t protocolNumber)
+{
+  // Assembly Ethernet header
+  EthernetHeader ethHeader;
+  ethHeader.SetSource (Mac48Address::ConvertFrom (source));
+  ethHeader.SetDestination (Mac48Address::ConvertFrom (dest));
+  ethHeader.SetLengthType (protocolNumber);
+
+  // Get queue index
+  Ipv4Header ipHeader;
+  packet->PeekHeader (ipHeader);
+  auto dscp = ipHeader.GetDscp ();
+  // Add Ethernet header
+  packet->AddHeader (ethHeader);
+  // Enqueue
+  m_queues[dscp].push (packet);
+  m_inQueueBytesList[dscp] += packet->GetSize ();
+  m_inQueuePacketsList[dscp]++;
+
+  m_nInQueueBytes += packet->GetSize ();
+  m_nInQueuePackets++;
+
+  return true; // Enqueue packet successfully
+}
+
+bool
+NoPfcSwitchPort::Receive (Ptr<Packet> p)
+{
+  NS_LOG_FUNCTION_NOARGS ();
+
+  m_nRxBytes += p->GetSize ();
+
+  PfcSwitchTag tag (m_dev->GetIfIndex ());
+  p->AddPacketTag (tag); // Add tag for tracing input device when dequeue in the net device
+
+  return true; // Forward up to node without any change
+}
+
+Ptr<Packet>
+NoPfcSwitchPort::DequeueRoundRobin (uint32_t &qIndex)
+{
+  NS_LOG_FUNCTION_NOARGS ();
+  if (m_nInQueuePackets == 0 || m_nQueues == 0)
+    {
+      NS_LOG_LOGIC ("Queues empty");
+      return 0;
+    }
+  else // Not control queues
+    {
+      for (uint32_t i = 0; i < m_nQueues; i++)
+        {
+          uint32_t qIdx = (m_lastQueueIdx + i) % m_nQueues;
+          if (CanDequeue (qIdx))
+            {
+              Ptr<Packet> p = m_queues[qIdx].front ();
+              m_queues[qIdx].pop ();
+
+              m_nInQueueBytes -= p->GetSize ();
+              m_nInQueuePackets--;
+              m_inQueueBytesList[qIdx] -= p->GetSize ();
+              m_inQueuePacketsList[qIdx]--;
+
+              m_lastQueueIdx = qIdx;
+              qIndex = qIdx;
+
+              return p;
+            }
+        }
+      return 0;
+    }
+}
+
+Ptr<Packet>
+NoPfcSwitchPort::Dequeue (uint32_t &qIndex)
+{
+  NS_LOG_FUNCTION_NOARGS ();
+
+  if (m_inQueuePacketsList[m_nQueues] == 0) // No control packets
+    {
+      return DequeueRoundRobin (qIndex);
+    }
+  else // Dequeue control packets without FCCL check
+    {
+      Ptr<Packet> p = m_queues[m_nQueues].front ();
+      m_queues[m_nQueues].pop ();
+
+      m_nInQueueBytes -= p->GetSize ();
+      m_nInQueuePackets--;
+      m_inQueueBytesList[m_nQueues] -= p->GetSize ();
+      m_inQueuePacketsList[m_nQueues]--;
+
+      qIndex = m_nQueues;
+      return p;
+    }
+}
+
+bool
+NoPfcSwitchPort::CanDequeue (uint32_t qIndex)
+{
+  NS_LOG_FUNCTION (qIndex);
+  // No packets in queue
+  if (m_inQueuePacketsList[qIndex] == 0)
+    return false;
+  // Packets in queue
+  return true;
+}
+
+void
+NoPfcSwitchPort::DoDispose ()
+{
+  NS_LOG_FUNCTION (this);
+  m_queues.clear ();
+  DpskNetDeviceImpl::DoDispose ();
+}
+
+} // namespace ns3
---
+++ src/pfc/model/nopfc-switch-port.h
@@ -0,0 +1,184 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2020 Nanjing University
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Yanqing Chen  <shellqiqi@outlook.com>
+ */
+
+#ifndef NOPFC_SWITCH_PORT_H
+#define NOPFC_SWITCH_PORT_H
+
+#include "ns3/dpsk-net-device-impl.h"
+#include "ns3/traced-callback.h"
+#include "ns3/cbfc-header.h"
+#include <vector>
+#include <queue>
+
+namespace ns3 {
+
+/**
+ * \ingroup pfc
+ * \class NoPfcSwitchPort
+ * \brief Plain Net Device Logic Implementation.
+ *
+ * Attention: No data packet modify on the port when receive (for mmu statics).
+ * Add Ethenet header when transmit.
+ */
+class NoPfcSwitchPort : public DpskNetDeviceImpl
+{
+public:
+  /**
+   * \brief Get the TypeId
+   *
+   * \return The TypeId for this class
+   */
+  static TypeId GetTypeId (void);
+
+  /**
+   * Constructor
+   */
+  NoPfcSwitchPort ();
+
+  /**
+   * Destructor
+   */
+  virtual ~NoPfcSwitchPort ();
+
+  /**
+   * Setup queues.
+   *
+   * \param n queue number
+   */
+  void SetupQueues (uint32_t n);
+
+  /**
+   * Clean up queues.
+   */
+  void CleanQueues ();
+
+  /**
+   * Transmit callback to notify mmu
+   *
+   * \param output device
+   * \param output packet
+   * \param output queue index
+   * \return void
+   */
+  typedef Callback<void, Ptr<NetDevice>, Ptr<Packet>, uint32_t> DeviceDequeueNotifier;
+
+  /**
+   * Setup dequeue event notifier
+   *
+   * \param h callback
+   */
+  void SetDeviceDequeueHandler (DeviceDequeueNotifier h);
+
+protected:
+  /**
+   * Switch port transmitting logic.
+   *
+   * \return Ptr to the packet.
+   */
+  virtual Ptr<Packet> Transmit ();
+
+  /**
+   * \param packet packet sent from above down to Network Device
+   * \param source source mac address (so called "MAC spoofing")
+   * \param dest mac address of the destination (already resolved)
+   * \param protocolNumber identifies the type of payload contained in
+   *        this packet. Used to call the right L3Protocol when the packet
+   *        is received.
+   *
+   *  Called by Switch Node.
+   *
+   * \return whether the Send operation succeeded
+   */
+  virtual bool Send (Ptr<Packet> packet, const Address &source, const Address &dest,
+                     uint16_t protocolNumber);
+
+  /**
+   * Switch port receiving logic.
+   *
+   * \param p Ptr to the received packet.
+   * \return whether need to forward up.
+   */
+  virtual bool Receive (Ptr<Packet> p);
+
+  /**
+   * \brief Dispose of the object
+   */
+  virtual void DoDispose (void);
+
+private:
+  /**
+   * Dequeue by round robin for queues of the port
+   * except the control queue.
+   *
+   * \param qIndex dequeue queue index (output)
+   * \return Ptr to the dequeued packet
+   */
+  Ptr<Packet> DequeueRoundRobin (uint32_t &qIndex);
+
+  /**
+   * Dequeue a packet for queues with the control queue.
+   *
+   * \param qIndex dequeue queue index (output)
+   * \return Ptr to the dequeued packet
+   */
+  Ptr<Packet> Dequeue (uint32_t &qIndex);
+
+  /**
+   * If can dequeue a packet from a queue.
+   *
+   * \param qIndex dequeue queue index
+   * \return true: can dequeue
+   */
+  bool CanDequeue (uint32_t qIndex);
+
+  uint32_t m_nQueues; //!< queue count of the port (control queue not included)
+  std::vector<std::queue<Ptr<Packet>>> m_queues; //!< queues of the port (with control queue)
+
+  uint32_t m_lastQueueIdx; //!< last dequeue queue index (control queue excluded)
+
+  DeviceDequeueNotifier m_mmuCallback; //!< callback to notify mmu
+
+public:
+  /// Statistics
+
+  uint64_t m_nInQueueBytes; //!< total in-queue bytes (control queue included)
+  std::vector<uint64_t> m_inQueueBytesList; //!< in-queue bytes in every queue
+
+  uint32_t m_nInQueuePackets; //!< total in-queue packet count (control queue included)
+  std::vector<uint32_t> m_inQueuePacketsList; //!< in-queue packet count in every queue
+
+  uint64_t m_nTxBytes; //!< total transmit bytes
+  uint64_t m_nRxBytes; //!< total receive bytes
+
+private:
+  /**
+   * Disabled method.
+   */
+  NoPfcSwitchPort &operator= (const NoPfcSwitchPort &o);
+
+  /**
+   * Disabled method.
+   */
+  NoPfcSwitchPort (const NoPfcSwitchPort &o);
+};
+
+} // namespace ns3
+
+#endif /* NOPFC_SWITCH_PORT_H */
