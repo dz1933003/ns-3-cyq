824f3f74362c2a124fe08233821e470a6e62775d
shell <shellqiqi@outlook.com>
2021-03-15 20:17:23 +0800

    add start index and seed to script

--- ns3_flowgen/traffic_gen.py
+++ ns3_flowgen/traffic_gen.py
@@ -30,14 +30,36 @@ if __name__ == "__main__":
         dest="cdf_file",
         help="the file of the traffic size cdf",
         default="uniform_distribution.txt",
+        type="string",
     )
-    parser.add_option("-n", "--nhost", dest="nhost", help="number of hosts")
+    parser.add_option(
+        "-n",
+        "--nhost",
+        dest="nhost",
+        help="number of hosts",
+        type="int",
+    )
+    parser.add_option(
+        "--sindex",
+        dest="start_index",
+        help="start index",
+        default=0,
+        type="int",
+    )
+    parser.add_option(
+        "--seed",
+        dest="seed",
+        help="random seed",
+        default=0,
+        type="int",
+    )
     parser.add_option(
         "-l",
         "--load",
         dest="load",
         help="the percentage of the traffic load to the network capacity, by default 0.3",
-        default="0.3",
+        default=0.3,
+        type="float",
     )
     parser.add_option(
         "-b",
@@ -45,13 +67,15 @@ if __name__ == "__main__":
         dest="bandwidth",
         help="the bandwidth of host link (G/M/K), by default 10G",
         default="10G",
+        type="string",
     )
     parser.add_option(
         "-t",
         "--time",
         dest="time",
         help="the total run time (s), by default 10",
-        default="10",
+        default=10,
+        type="float",
     )
     parser.add_option(
         "-o",
@@ -59,23 +83,26 @@ if __name__ == "__main__":
         dest="output",
         help="the output file",
         default="tmp_traffic.csv",
+        type="string",
     )
     options, args = parser.parse_args()
 
     # global members
     base_time = 1e9  # ns
-    cdf_rand = CdfRand(0)
-    possion_rand = PossionRand(0)
-    host_rand = random.Random(0)
+    seed = options.seed
+    cdf_rand = CdfRand(seed)
+    possion_rand = PossionRand(seed)
+    host_rand = random.Random(seed)
 
     # read options
     if not options.nhost:
         print("Error: please use -n to enter number of hosts")
         sys.exit(0)
-    n_host = int(options.nhost)  # host number
-    load = float(options.load)  # traffic load
+    n_host = options.nhost  # host number
+    start_index = options.start_index  # host start index
+    load = options.load  # traffic load
     bandwidth = get_bps_from_str(options.bandwidth)
-    time = float(options.time) * 1e9  # convert to ns
+    time = options.time * 1e9  # convert to ns
     output = options.output
     if bandwidth == None:
         print("Error: bandwidth format incorrect")
@@ -104,16 +131,16 @@ if __name__ == "__main__":
     avg_interval_arrival = 1 / (bandwidth * load / 8.0 / avg) * 1e9  # ns
     host_list = [
         (base_time + int(possion_rand.get_rand(avg_interval_arrival)), i)
-        for i in range(n_host)
+        for i in range(start_index, start_index + n_host)
     ]  # init host with (first sending time, host id)
     heapq.heapify(host_list)
     flow_dict = dict()
     while len(host_list) > 0:
         start_time, src_host_id = host_list[0]
         next_time = start_time + int(possion_rand.get_rand(avg_interval_arrival))
-        dst_host_id = host_rand.randint(0, n_host - 1)
+        dst_host_id = host_rand.randint(start_index, start_index + n_host - 1)
         while dst_host_id == src_host_id:
-            dst_host_id = host_rand.randint(0, n_host - 1)
+            dst_host_id = host_rand.randint(start_index, start_index + n_host - 1)
         if next_time > time + base_time:
             heapq.heappop(host_list)
         else:
@@ -126,7 +153,7 @@ if __name__ == "__main__":
                 flow_dict[(src_host_id, dst_host_id)] = 1
             # Notice that time is (s) and size is (byte) by default
             output_file.write(
-                "%dns,%d,%d,%d,%d,%dB,%d\n"
+                "%dns,h%d,h%d,%d,%d,%dB,%d\n"
                 % (
                     start_time,
                     src_host_id,
