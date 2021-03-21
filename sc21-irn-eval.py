593ddc96e37d1f712a170a816933c35df5abf1fc
cyq <dz1933003@smail.nju.edu.cn>
2021-03-21 18:20:51 +0800

    add confs

--- sc21-irn-eval.py
+++ sc21-irn-eval.py
@@ -2,7 +2,22 @@
 
 from subprocess import Popen
 
-Popen(
-    './waf --run "scratch/pfc-cyq ns3_config/irn/interDC/inter-dc-config.json"',
-    shell=True,
-)
+dirs = ["acrossDC", "interDC"]
+conf = ["inter-dc-config", "across-dc-config"]
+suffix = ["20", "40", "60", "80", "100"]
+ps = []
+
+for d in dirs:
+    for c in conf:
+        for i in range(20, 120, 20):
+            cmd = f'./waf --run "scratch/pfc-cyq ns3_config/irn/{d}/{c}-{i}.json {i}"'
+            print(cmd)
+            ps.append(
+                Popen(
+                    cmd,
+                    shell=True,
+                )
+            )
+
+for p in ps:
+    p.wait()
